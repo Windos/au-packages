9596ad4436f2d3f4f798f9697fd4cb1016890f1a
Chocolatey <chocolatey@realdimensions.net>
2022-07-08 13:19:27 +0000

    AU: 1 updated - xca [skip ci] https://gist.github.com/03903cd565061a825f153ce2f9d6227c/a65e7c6441d120bef8efb40f6c52f31a1dca7b1d

--- xca/tools/chocolateyinstall.ps1
+++ xca/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
   packageName            = "$env:chocolateyPackageName"
   FileType               = 'msi'
   SilentArgs             = '/qn /norestart'
-  url                    = 'https://github.com/chris2511/xca/releases/download/RELEASE.2.4.0/xca-2.4.0.msi'
-  checksum               = '3479fe69feed931ce4e8a9945c6cc30b0f1a947980a2288fdbdd7faf1d78ec59'
+  url                    = 'https://github.com/chris2511/xca/releases/download/RELEASE.2.3.0/xca-2.3.0.msi'
+  checksum               = 'a3d2295af4720455f20c366bd5c2c5a50625abab97b0209618e8a1e3c3c71168'
   checksumType           = 'sha256'
 }
 Install-ChocolateyPackage @packageArgs
