7e42528e4b7c89a64d2bfd64fe99a6a487e87778
Chocolatey <chocolatey@realdimensions.net>
2022-11-09 21:10:23 +0000

    AU: 1 updated - sdrsharp [skip ci] https://gist.github.com/03903cd565061a825f153ce2f9d6227c/baf7bb544c0becfa5275400773f6ac6d5f51eeb2

--- sdrsharp/tools/chocolateyinstall.ps1
+++ sdrsharp/tools/chocolateyinstall.ps1
@@ -3,8 +3,8 @@
   url                    = 'https://airspy.com/downloads/sdrsharp-x86.zip'
   url64bit               = 'https://airspy.com/downloads/sdrsharp-x64.zip'
   UnzipLocation          = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
-  checksum               = 'bdc9898bf2d6c21a8b27999937a9103b09269e5cb0686e6619d0c3cd58e8d6cc'
-  checksum64             = '48387fc16d1c0089a67ac5bcdb74e110ad89d43ae8ed4c2d735c71d5f0fad6fb'
+  checksum               = '475e5564b1d91c095e80cd84ab85718e88c9aea370add435c5203545d1b0d49a'
+  checksum64             = '7d4c6a91d34839fa13af2d432ae7296864a53172c86c0e5c84464268b65a7c69'
   checksumType           = 'sha256'
   checksumType64         = 'sha256'
 }
